338963c3b8e98378a0a05779fa9b29c144c02c29
Cuongpahm2008 <159127227+Cuongpahm2008@users.noreply.github.com>
2024-02-06 17:23:31 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+//====Nhập Thông Tin Của Bạn Vào Đây==============
+
+ImgLink_Background = "https://drive.google.com/uc?export=download&id=17OooPmrnjzDZKHMoWLtWiAOoWnu7Odpf"
+ImgLink_Avatar = "https://drive.google.com/uc?export=download&id=1RU1MqMi1ZrghI0CK7N-DKZBW7xLGxhPt"
+Nickname_Profile = "Karasuma"
+RealName_Profile = "Vinh Trần"
+LinkInformation = [
+    Facebook_LinkProfile = "https://www.facebook.com/vinhtran.karasuma.206/",
+    Discord_LinkProfile = "KarasumaVinh#4793",
+    TikTok_LinkProfile = "https://www.tiktok.com/@rin206_geip",
+    Instagram_LinkProfile = "https://www.instagram.com/karasumavinh"
+]
